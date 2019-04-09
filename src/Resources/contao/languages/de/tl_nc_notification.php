0d3e0f2d90efe64883a3ef9dc0eff56ffb521f9a
Alexander Naumov <me@alexandernaumov.de>
2019-04-09 16:30:59 +0200

    add labels for zammad palette

---
+++ src/Resources/contao/languages/de/tl_nc_notification.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['zammad_legend'] = 'Zammad-Einstellungen';
+$GLOBALS['TL_LANG']['tl_nc_notification']['zammad_group'] = [ 'Zammad Gruppe', 'Bitte geben Sie eine Zammad Gruppe ein.' ];
