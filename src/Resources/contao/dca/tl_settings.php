77d9096572002f8553c3d97b0d16e0895535e29c
Alexander Naumov <me@alexandernaumov.de>
2019-04-26 16:32:01 +0200

    add auth sub palette

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,6 +1,30 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{zammad_settings},zammadHost,zammadUser,zammadPassword';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['__selector__'][] = 'zammadAuthType';
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{zammad_settings},zammadAuthType';
+$GLOBALS['TL_DCA']['tl_settings']['subpalettes']['zammadAuthType_basic'] = 'zammadHost,zammadUser,zammadPassword';
+$GLOBALS['TL_DCA']['tl_settings']['subpalettes']['zammadAuthType_token'] = 'zammadToken';
+
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['zammadAuthType'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['zammadAuthType'],
+    'inputType' => 'radio',
+    'eval' => [
+        'tl_class' => 'clr',
+        'submitOnChange' => true,
+    ],
+    'options' => [ 'basic', 'token' ],
+    'reference' => &$GLOBALS['TL_LANG']['tl_settings']['reference']['zammadAuthType'],
+    'exclude' => true
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['zammadToken'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['zammadToken'],
+    'inputType' => 'text',
+    'eval' => [
+        'tl_class' => 'w50'
+    ]
+];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['zammadHost'] = [
     'label' => &$GLOBALS['TL_LANG']['tl_settings']['zammadHost'],
