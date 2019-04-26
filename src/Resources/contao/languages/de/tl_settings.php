6234f20261bdc7c3cbaa5817ce99d097e3db7c08
Alexander Naumov <me@alexandernaumov.de>
2019-04-26 16:32:09 +0200

    add labels

--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -1,7 +1,10 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_settings']['zammad_settings'] = 'Zammad-Einstellungen';
-
+$GLOBALS['TL_LANG']['tl_settings']['zammadAuthType'] = [ 'Authentication Type', 'Bitte wählen Sie einen authentication type aus.' ];
 $GLOBALS['TL_LANG']['tl_settings']['zammadHost'] = [ 'Host', 'Bitte geben Sie die URL der verwendeten Zammad Instanz ein z.B https://yourname.zammad.com' ];
 $GLOBALS['TL_LANG']['tl_settings']['zammadUser'] = [ 'Benutzer', 'Bitte geben Sie Ihren zammad Benutzer ein.' ];
 $GLOBALS['TL_LANG']['tl_settings']['zammadPassword'] = [ 'Passwort', 'Bitte geben Sie Ihren zammad Passwort ein.' ];
+$GLOBALS['TL_LANG']['tl_settings']['zammadToken'] = [ 'Token', 'Bitte geben Sie Ihr zammad token ein.' ];
+$GLOBALS['TL_LANG']['tl_settings']['reference']['zammadAuthType']['token'] = 'Token';
+$GLOBALS['TL_LANG']['tl_settings']['reference']['zammadAuthType']['basic'] = 'HTTP Basic Authentication';
